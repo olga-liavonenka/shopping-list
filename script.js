42dc5200af9a755e606276faf44539cc64599375
olga-liavonenka <olga95_kon@mail.ru>
2023-04-06 13:15:48 +0200

    implemented task-4

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
+const inputElement = document.querySelector('#input');
+
+inputElement.addEventListener('keydown', function(event){
+
+  if (event.key == 'Enter') {
+    if(inputElement.value != '') {
+      const newElement = document.createElement('li');
+      newElement.textContent = inputElement.value;
+      
+      newElement.addEventListener('click', function() {
+        newElement.classList.toggle('done');
+      });
+
+      const itemsContainer = document.querySelector('.items');
+      itemsContainer.append(newElement);
+
+      inputElement.value = '';
+  } }
+
+} );
+
+
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
@@ -7,3 +29,4 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
